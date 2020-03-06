b8e5f0147b5b172fc53a702b579c7ef49f0b93f7
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2020-03-07 01:15:14 +0300

    check_events: whole script refactoring

--- susp/check_events.py
+++ susp/check_events.py
@@ -1,30 +1,67 @@
+#! /usr/bin/env python
+
 """Main parsing script."""
 
-import susp.db
+import logging
+
 import susp.parser
+import susp.utils
+
+from mongoengine.errors import DoesNotExist
+
 from susp.event import Event
+
+LOG = susp.utils.make_logger()
 
 
 def main():
+    """Main parsing function."""
+
+    LOG.info("Parsing events...")
+    # TODO: make calls with different user agent and maybe sometimes use proxy
     events = susp.parser.get_all_events()
+
+    processed_events_count = 0
+    new_events_count = 0
+    became_available_events_count = 0
 
     for event in events:
         datetime_str = susp.parser.get_event_datetime_str(event)
         if not datetime_str:
+            LOG.error(f"Couldn't fetch datetime_str for event: {event}, skipping...")
             continue
 
-        price = get_event_price(event)
-        poster_url = get_event_poster_url(event)
-        is_available = check_event_availability(event)
-        seats_left = fetch_remaining_tickets(event)
-        url = get_event_url(event)
+        is_available = susp.parser.check_event_availability(event)
 
-        # existing_event = get_event_from_db(datetime_str)
-        saved_event = Event.one(datetime_str)  # checking if event exists in db, if exists, returns event object
-        if saved_event:
-            saved_event.update_availability(is_available)  # this func calls notification func if needed
-        else:
-            Event.create(datetime_str, price, poster_url, is_available, seats_left, url)
+        try:
+            saved_event = Event.objects.get(datetime_str=datetime_str)
+            if is_available and not saved_event.is_available:
+                susp.notifications.available_again(saved_event)
+                became_available_events_count += 1
+
+        except DoesNotExist:
+            price = susp.parser.get_event_price(event)
+            poster_url = susp.parser.get_event_poster_url(event)
+            seats_left = susp.parser.get_remaining_tickets(event)
+            url = susp.parser.get_event_url(event)
+
+            event = Event(
+                datetime_str=datetime_str,
+                price=price,
+                poster_url=poster_url,
+                is_available=is_available,
+                seats_left=seats_left,
+                url=url,
+            )
+            susp.notifications.new_event(event)
+            new_events_count += 1
+
+        processed_events_count += 1
+
+    LOG.info("Parsing finished")
+    LOG.info(f"Processed events:        {processed_events_count}")
+    LOG.info(f"New events:              {new_events_count}")
+    LOG.info(f"Events available again:  {became_available_events_count}")
 
 
 if __name__ == "__main__":
