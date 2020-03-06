01ac8f6c22843d15b9066c8745ffbbdd52d17d29
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2020-03-07 01:12:25 +0300

    event: create event model based on mongoengine document

--- susp/event.py
+++ susp/event.py
@@ -1,24 +1,26 @@
 """Describes event model."""
 
-from susp.notifications import post_to_channel
+from mongoengine import (
+    connect,
+    BooleanField,
+    Document,
+    IntField,
+    StringField,
+)
+from mongoengine.errors import NotUniqueError, DoesNotExist
+
+# db connection
+connect('susp')
 
 
-class Event():
+class Event(Document):
+    """Represents event document."""
 
-    def __init__(self):
-        """Check as in c2."""
-        pass
+    meta = {'collection': 'events'}
 
-    def create(self, datetime_str, price, poster_url, is_available, seats_left, url):
-        """Actions on event creation."""
-        message = ""
-        post_to_channel(message)  # sending notification about new event
-        pass
-
-    def one(self):
-        """Returns event as instance of class from db or None if event doesn't exist."""
-        pass
-
-    def update_availability(self, is_available):
-        """Notifies if event were unavailable and became available."""
-        pass
+    datetime_str = StringField(required=True, unique=True)
+    price = IntField()
+    poster_url = StringField()
+    is_available = BooleanField()
+    seats_left = IntField()
+    url = StringField()
