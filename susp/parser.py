492a07f564a4d5dbfd53d31cc7f2263e895d1a88
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2020-03-20 00:17:40 +0300

    parser: get rid of unnecessary comments and impove naming

--- susp/parser.py
+++ susp/parser.py
@@ -7,98 +7,13 @@ from bs4 import BeautifulSoup
 from bs4 import Comment
 
 
-# playing with user agents
-chrome_ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
-safari_ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"
-headers = {"User-Agent": safari_ua}
-
-
-# getting page
-url = "http://standupstore.ru/"
-response = requests.get(url, headers=headers)
-html = response.content
-
-
-# actual parsing
-soup = BeautifulSoup(html,'html.parser')
-
-events = soup.findAll('div', class_="t778__col")
-event1 = events[0]
-event2 = events[1]
-event3 = BeautifulSoup("""<div class="t778__mark">5 мест</div>""", "html.parser")
-
-"""
-### Чтобы парсер заработал, надо
-
-#### Найти все мероприятия на сайте
-1. первый способ: найти все блоки класса "t778__col"
-        events = soup.findAll('div', class_="t778__col")
-2. второй способ: ??? - нужно еще посмотреть для надежности
-        проверить классы t_col t_col_4 t_item js
-
-
-#### Вытащить информацию о каждом мероприятии
-
-1. гарантированно находить дату и время события, т.к. это будет идентификатор мероприятия  
-    сейчас это хранится в пяти местах, может искать каждое по очереди, если не найдены дата или время
-    1. первый способ: через поле data-date в закоментированной строке
-    2. второй способ: через div t778__descr - блок, в котором лежит инфа, которая отображается на странице
-            data-date="24 февраля, 21:30"
-    строку можно превратить в datetime  
-
-2. находить стоимость мероприятия
-    1. первый способ: находить блок с ценой, но этого блока может не быть
-            if event1.find('div', class_="js-product-price"):
-                event_price = event1.find('div', class_="js-product-price").text
-    2. второй способ:
-            comment = event2.find(string=lambda text: isinstance(text, Comment))
-            comment_soup = BeautifulSoup(comment, 'html.parser')
-            event_price = comment_soup.find('a')['data-cost']
-
-3. находить и сохранять афишу мероприятия
-    1. первый способ: находить блок с картинкой и брать поле data-original
-            event1.find('div', class_="js-product-img")["data-original"]
-    2. второй способ: находить блок с картинкой, брать поле style и каким-то образом вытаскивать оттуда
-    3. третий и четыверный способы аналогичны, но для дублирующего блока с картинками, пока нет смысла расписывать
-    
-4. находить и сохранять доступность билетов в виде булевого значения
-    1. первый способ (желаемый): через поле data-seats в закоментированной строке-ссылке, если мест 0, то билетов нет. Минус в том, что пока не понятно, всегда ли есть и будет закоментированная строка-ссылка
-    2. второй способ: через слова "Места есть", "Мест нет" — минус в том, что иногда формулировка может отличаться, например "Осталось 3 места"
-
-5. находить и сохранять количество оставшихся мест, если указаны
-    1. первый способ (желаемый): через поле data-seats в закоментированной строке-ссылке, если мест 0, то билетов нет. Минус в том, что пока не понятно, всегда ли есть и будет закоментированная строка-ссылка
-
-6. находить и сохранять ссылку на мероприятие, если ссылка существует
-    1. первый способ: брать первую ссылку из мероприятия, т.к. пока не нашел случаев, когда есть другие ссылки
-
-примерная схема работы
-
-import susp.db
-import susp.events
-
-html = ...
-soup = ...
-events = susp.events.all()
-
-for event in events:
-    when = get_event_datetime(event)                # if nothing found leaving this event
-    if not when:
-        continue
-    price = get_event_price(event)                  # returns None if can't fetch
-    poster_url = get_event_poster_url(event)        # returns None if can't fetch
-    is_available = check_event_availability(event)  # returns None if can't fetch
-    seats_left = fetch_remaining_tickets(event)     # returns None if can't fetch
-    event_url = get_event_url(event)                # returns None if can't fetch
-
-    susp.db.save_event(when, price, poster_url, is_available, seats_left)
-    # saves if event is new or updates if it's existing event
-    # also can have some logic like if availability has changed from False to True
-    # also need to place somewhere logic of notifying about new events
-"""
-
-# automating parsing of all pages
 def get_all_events():
     """Iterates pages and returns events from all pages."""
+
+    # playing with user agents
+    chrome_ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    safari_ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"
+    headers = {"User-Agent": safari_ua}
 
     total_events = []
     page_num = 1
@@ -182,7 +97,7 @@ def get_event_poster_url(event):
     return event_poster_url
 
 
-def is_available(event):
+def get_event_availability(event):
     """Returns availability of given event."""
 
     is_available = None
