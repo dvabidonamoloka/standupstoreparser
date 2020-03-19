53bb802b79da235b317a9dee7d3566af437a412a
Odintsov Timur <timodin@yandex.ru>
2020-03-19 04:42:30 +0300

    notifications: add generate_event_message() func

--- susp/notifications.py
+++ susp/notifications.py
@@ -15,3 +15,32 @@ def post_to_channel(message):
     mybot.send_message(chat_id=settings.CHAT_ID, text=message, parse_mode='Markdown')
 
     LOG.info('Message posted to the channel')
+
+
+def generate_event_message(event, is_new):
+    '''
+    Creates message to notificate about new event or
+    about the newly available event.
+    '''
+
+    LOG.debug('Generating event message')
+
+    if event.url or event.poster_url:
+        date = event.datetime_str or 'Уточните дату на сайте'
+        price = event.price or 'Уточните цену на сайте'
+        seats = event.seats_left or 'Неизвестно'
+        poster_url = event.poster_url  # if poster_url is None, space symbol will be ignored by telegram
+        url = event.url or 'https://standupstore.ru/'
+
+        message = f'Дата:[ ]({poster_url}){date}\nЦена: {price}\nОсталость мест: {seats}\n[Купить билеты]({url})'
+        if is_new:
+            message = 'Билеты снова в продаже!\n' + message
+            
+        LOG.debug('Message created')
+    else:
+        LOG.error('Unable to generate a message: event url and poster url are missing')
+        message = None
+    
+    return message
+
+
