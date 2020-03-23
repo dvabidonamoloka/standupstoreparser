fa843c0df6b76b31791a4cf0941fc758f43befa2
Odintsov Timur <timodin@yandex.ru>
2020-03-23 04:27:30 +0300

    utils: add email handler to logger

--- susp/utils.py
+++ susp/utils.py
@@ -1,14 +1,35 @@
 import logging
+import logging.handlers
+
+from susp import settings
 
 
 def make_logger():
-    '''Creats and sets logger configurations'''
+    '''
+    Creats and sets logger configurations.
+
+    Note: use LOG.error(message, exc_info=True) or 
+    LOG.exception(message) to log traceback
+    '''
 
     logger = logging.getLogger('susp')
     logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler('/var/log/susp.log')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh = logging.FileHandler('susp.log')
+    # TODO: make mutable subject in SMTPHandler to know right away what kind of error came
+    mh = logging.handlers.SMTPHandler(
+        mailhost=(settings.SMPT_SERVER, settings.SMPT_PORT),
+        fromaddr=settings.EXCEPTIONS_EMAIL_FROM,
+        toaddrs=settings.EXCEPTIONS_EMAIL_TO,
+        subject='Standupstore parser catched an exception',
+        credentials=(settings.EXCEPTIONS_EMAIL_FROM, settings.SMPT_PASSWORD),
+        secure=()
+    )
+    mh.setLevel(logging.ERROR)
+    formatter = logging.Formatter('%(asctime)s - %(pathname)s - %(levelname)s - %(message)s')
+    exception_formatter = logging.Formatter('%(asctime)s - %(pathname)s - %(message)s')
     fh.setFormatter(formatter)
+    mh.setFormatter(exception_formatter)
     logger.addHandler(fh)
+    logger.addHandler(mh)
 
     return logger
