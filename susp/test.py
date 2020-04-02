007847d1cb6c032361a5ba2e9bc0cfe08a6b5984
Odintsov Timur <timodin@yandex.ru>
2020-04-03 00:56:00 +0300

    test commit

---
+++ susp/test.py
@@ -0,0 +1,7 @@
+import susp.notifications
+
+def any_func(a):
+    return a * 2 # sdfgdgkjnedgndsfkjgnrkjgnkjrsngkjrngkjsrndgvkjnerjkgnvkjersngkjesrnkgjnerkjngkjersngkjnerskjgnskejrngkjersngkjnerjkgnesjkrngkjerngkjserjge
+
+def foo(b):
+    return b.upper()
